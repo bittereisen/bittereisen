7b10132b7debd1fc5fddab9ad6aabe85d9f66a52
bittereisen <bittereisen@gmail.com>
2025-02-28 11:03:11 +0000

    updates to index.html

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,8 @@ admin.initializeApp();
  * @param {Object} req The request object
  * @param {Object} res The response object
  */
-exports.submitEmail = functions.https.onRequest((req, res) => {
+
+exports.submitEmail = functions.https.onRequest(async (req, res) => {
   console.log('Function is being deployed!');
 
   // Allow only POST requests (no CORS handling here)
@@ -22,18 +23,20 @@ exports.submitEmail = functions.https.onRequest((req, res) => {
     return res.status(400).send('Invalid email');
   }
 
+  // Use try-catch around the Firestore write operation
   try {
-    // Save email to Firestore
-    admin.firestore().collection('emails').add({
+    await admin.firestore().collection('emails').add({
       email: email,
       timestamp: admin.firestore.FieldValue.serverTimestamp(),
     });
     res.status(200).send('Email submitted successfully');
   } catch (error) {
+    // Log the error and return 500 Internal Server Error
     console.error('Error writing document: ', error);
     res.status(500).send('Internal Server Error');
   }
 });
+
 
 /**
  * Function to validate email format.
